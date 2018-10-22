5292dd03dad1d0686a9bc246c5140c33b00a5a52
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2018-10-22 19:23:29 +0300

    file_part_send. Changed timer count realization

--- src/part_sender.cpp
+++ src/part_sender.cpp
@@ -15,12 +15,6 @@
  * limitations under the License.
  */
 
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <atomic>
-#include <chrono>
-
 #include "staticlib/config.hpp"
 #include "staticlib/utils.hpp"
 #include "staticlib/tinydir.hpp"
@@ -38,48 +32,6 @@
 
 namespace wilton {
 namespace http {
-
-namespace {
-class wait_timer
-{
-    std::mutex cond_mtx;
-    std::atomic_bool stop_flag;
-    std::condition_variable cond;
-    std::thread waiter;
-    bool is_expired_flag;
-public:
-    wait_timer() : is_expired_flag(false) {
-        this->stop_flag.exchange(false, std::memory_order_acq_rel);
-    }
-    ~wait_timer() {
-        stop();
-    }
-    bool is_expired() {
-        return is_expired_flag;
-    }
-    void start(int64_t milliseconds){
-        is_expired_flag = false;
-        waiter = std::thread([this, milliseconds] () {
-            this->stop_flag.exchange(false, std::memory_order_acq_rel);
-            std::unique_lock<std::mutex> lck(cond_mtx);
-            while (!this->stop_flag) {
-               std::cv_status status = this->cond.wait_for(lck, std::chrono::milliseconds(milliseconds));
-               if (std::cv_status::timeout == status) {
-                   this->is_expired_flag = true;
-                   break;
-               }
-            }
-        });
-    }
-    void stop(){
-        if (waiter.joinable()) {
-            stop_flag.exchange(true, std::memory_order_acq_rel);
-            cond.notify_all();
-            waiter.join();
-        }
-    }
-};
-}
 
 const std::string part_sender::opt_chunk_number{"X-Wilton-FileUpload-ChunkNumber"};
 const std::string part_sender::opt_standart_chunk_size{"X-Wilton-FileUpload-ChunksMaxSizeBytes"};
@@ -125,17 +77,10 @@ std::string wilton::http::part_sender::send_file(bool& is_timer_expired){
     options.headers.push_back(header_option(opt_chunk_number, sl::support::to_string(chunk_number)));
 
     std::vector<sl::json::field> array_value;
-    wait_timer timer;
-    timer.start(send_options.timeout_ms);
+    size_t current_time = 0;
     while(chunk_number < send_options.chunks_count){
+        bool send_continue = true;
         options.headers.back() = (header_option(opt_chunk_number, sl::support::to_string(chunk_number)));
-        bool send_continue = false;
-        if (timer.is_expired()) {
-            sl::json::field expired_field("timer_expired", true);
-            array_value.push_back(std::move(expired_field));
-            is_timer_expired = true;
-            break;
-        }
         std::vector<sl::json::value> chunk_send_results;
         do {
             auto tpath = sl::tinydir::path(send_options.loaded_file_path);
@@ -149,8 +94,7 @@ std::string wilton::http::part_sender::send_file(bool& is_timer_expired){
             }
             options.request_body_content_length = static_cast<uint32_t>(expected_readed);
 
-            auto limited = sl::io::make_limited_source (source_in, expected_readed);
-
+            auto limited = sl::io::make_limited_source(source_in, expected_readed);
             try{
                 auto tmp_resp_resp = http->open_url(send_options.url, std::move(limited), options);
                 send_continue = !tmp_resp_resp.connection_successful();
@@ -161,8 +105,10 @@ std::string wilton::http::part_sender::send_file(bool& is_timer_expired){
                     sl::io::copy_all(tmp_resp_resp, sink);
                 }
                 data_hex = dest.get_string();
+                auto info = tmp_resp_resp.get_info();
+                current_time += static_cast<uint32_t> (info.total_time_secs*1000);
                 auto resp_json = wilton::http::client_response::to_json(std::move(data_hex),
-                        tmp_resp_resp, tmp_resp_resp.get_info());
+                        tmp_resp_resp, info);
                 chunk_send_results.push_back(std::move(resp_json));
             } catch (const std::exception& e) {
                 //supress error on send
@@ -171,7 +117,7 @@ std::string wilton::http::part_sender::send_file(bool& is_timer_expired){
                 chunk_send_results.push_back(std::move(tmp_val));
                 send_continue = true;
             }
-            if (timer.is_expired()) {
+            if (current_time >= send_options.timeout_ms) {
                 break;
             }
         } while (send_continue);
@@ -179,6 +125,12 @@ std::string wilton::http::part_sender::send_file(bool& is_timer_expired){
         sl::json::field chunk_field(sl::support::to_string(chunk_number), std::move(chunk_result));
         array_value.push_back(std::move(chunk_field));
         ++chunk_number;
+        if (current_time >= send_options.timeout_ms && ( send_continue || chunk_number != send_options.chunks_count)) {
+            sl::json::field expired_field("timer_expired", true);
+            array_value.push_back(std::move(expired_field));
+            is_timer_expired = true;
+            break;
+        }
     }
 
     sl::json::value result(std::move(array_value));
